65c37f1ced3429dc18f9d02a86bb91a9ff08b6f4
ullizen <andersson.au@gmail.com>
2022-09-18 13:44:39 +0200

    update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/ullis.cool",
+        }
+    }
 };
 
 export default config;
